e49da5d8cb5ae4b1f53b1539597e238400bda432
Steve Dever <stephendever.cr@gmail.com>
2021-04-26 13:30:23 -0400

    add more questions/code to index.js file

--- index.js
+++ index.js
@@ -8,13 +8,49 @@ inquirer.prompt([
     validate: (value) => { if (value) { return true } else { return "Please enter a value to continue" } },
   },
 
+  {
+    type: 'input',
+    message: 'Please enter your project description?',
+    name: 'projDescription',
+    validate: (value) => { if (value) { return true } else { return "Please enter a value to continue" } },
+  },
+
+  {
+    type: 'input',
+    message: 'Please describe the Installation',
+    name: 'installation',
+    validate: (value) => { if (value) { return true } else { return "Please enter a value to continue"  } },
+  },
+
+  {
+    type: 'input',
+    message: 'Please describe the usage',
+    name: 'usage',
+    validate: (value) => { if (value) { return true } else { return "Please enter a value to continue"  } },
+  },
+
+  {
+    type: 'input',
+    message: 'Please list your Contributors',
+    name: 'contributors',
+    validate: (value) => { if (value) { return true } else { return "Please enter a value to continue"  } },
+  },
+
 ]
 
 ).then((data) => {
   const filename = `README.md`;
   const readMe = `
-  # ${data.projTitle}
- }`;
+  # ${data.projTitle} / ${data.license} License
+
+  # Description
+  # ${data.projDescription}
+
+  # Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contribution](#contribution)
+`;
 
   console.log(readMe);
   fs.writeFile(filename, readMe, (err) =>
